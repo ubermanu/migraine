1002d6611d0ab2104ab9eb98bf80495c21185496
ubermanu <e.vodor@gmail.com>
2020-10-11 17:13:32 +0200

    Add merge processor

---
+++ src/Processor/MergeProcessor.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Migraine\Processor;
+
+use Migraine\Exception\StorageException;
+use Migraine\TaskRuntime;
+
+/**
+ * Class MergeProcessor
+ *
+ * @method array getMerging()
+ * @method $this setMerging(array $storages)
+ * @method string getStorage()
+ * @method $this setStorage(string $storage)
+ *
+ * @package Migraine\Processor
+ */
+class MergeProcessor extends AbstractProcessor
+{
+    /**
+     * @var array
+     */
+    protected array $data = [
+        'storage' => null,
+        'merging' => null,
+    ];
+
+    /**
+     * @inheritdoc
+     * @throws StorageException
+     */
+    public function execute(TaskRuntime $taskRuntime): void
+    {
+        $storage = $this->getStorageOrDefault($taskRuntime, 'storage');
+
+        foreach ($this->getMergingStorages() as $identifier) {
+            foreach ($taskRuntime->getStorage($identifier) as $row) {
+                $storage->add($row);
+            }
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getMergingStorages(): array
+    {
+        $identifiers = $this->getData('merging') ?? [];
+
+        if (is_string($identifiers)) {
+            $identifiers = [$identifiers];
+        }
+
+        return $identifiers;
+    }
+}
---
+++ tests/Processor/MergeProcessorTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Migraine\Tests\Processor;
+
+use Migraine\Exception\StorageException;
+use Migraine\Exception\TaskException;
+use Migraine\Processor\MergeProcessor;
+use Migraine\Processor\SelectProcessor;
+use Migraine\Storage;
+
+/**
+ * Class MergeProcessorTest
+ * @package Migraine\Tests\Processor
+ */
+class MergeProcessorTest extends AbstractProcessorTest
+{
+    /**
+     * @throws StorageException
+     * @throws TaskException
+     */
+    public function testCanMergeMultipleStorages(): void
+    {
+        $data = [
+            ['id' => 1, 'name' => 'John Doe'],
+            ['id' => 2, 'name' => 'John Doe'],
+        ];
+
+        $this->migraine->addStorage('test-1', new Storage($data));
+        $this->migraine->addStorage('test-2', new Storage($data));
+
+        $p = new MergeProcessor();
+        $p->setMerging(['test-1', 'test-2']);
+
+        $this->defaultTask->addProcessor($p);
+        $r = $this->migraine->execute();
+
+        $this->assertCount(2, $r->getStorage('test-1'));
+        $this->assertCount(2, $r->getStorage('test-2'));
+        $this->assertCount(4, $r->getDefaultStorage());
+    }
+}
