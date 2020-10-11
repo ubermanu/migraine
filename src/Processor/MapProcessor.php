75ef53c3686e704b7da09679e4459f96f0c4b14a
ubermanu <e.vodor@gmail.com>
2020-10-11 14:03:17 +0200

    Add map processor

---
+++ src/Processor/MapProcessor.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Migraine\Processor;
+
+use Migraine\Exception\StorageException;
+use Migraine\TaskRuntime;
+
+/**
+ * Class MapProcessor
+ * @package Migraine\Processor
+ */
+class MapProcessor extends AbstractProcessor
+{
+    /**
+     * @inheritdoc
+     * @throws StorageException
+     */
+    public function execute(TaskRuntime $taskRuntime): void
+    {
+        foreach ($this->getStorageOrDefault($taskRuntime, 'in') as &$record) {
+
+            // Backup current record
+            $baseRecord = $record;
+
+            // Apply mapping
+            foreach ($this->getMapping() as $key => $newKey) {
+                if (array_key_exists($key, $baseRecord)) {
+                    $record[$newKey] = $baseRecord[$key];
+                    if ($key !== $newKey) {
+                        unset($record[$key]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getMapping(): array
+    {
+        return $this->options['map'];
+    }
+}
---
+++ tests/Processor/MapProcessorTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Migraine\Tests\Processor;
+
+use Migraine\Exception\StorageException;
+use Migraine\Exception\TaskException;
+use Migraine\Processor\MapProcessor;
+use Migraine\Storage;
+
+/**
+ * Class MapProcessorTest
+ * @package Migraine\Tests\Processor
+ */
+class MapProcessorTest extends AbstractProcessorTest
+{
+    /**
+     * @throws StorageException
+     * @throws TaskException
+     */
+    public function testRenameTwoColumnNames(): void
+    {
+        $data = [
+            ['id' => 1, 'name' => 'John Doe'],
+        ];
+
+        $this->migraine->addStorage('test', new Storage($data));
+
+        $p = new MapProcessor([
+            'map' => [
+                'id' => 'userId',
+                'name' => 'firstName',
+            ],
+            'in' => 'test',
+        ]);
+
+        $this->defaultTask->addProcessor($p);
+        $r = $this->migraine->execute();
+
+        $this->assertEquals(
+            [
+                ['userId' => 1, 'firstName' => 'John Doe']
+            ],
+            $r->getStorage('test')->toArray()
+        );
+    }
+}
